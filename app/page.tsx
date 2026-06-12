7ac9654666948e6ceafb83401fe6efc98bf4ee67
joaoviitordev <joaovitorgomesdefaria@gmail.com>
2026-06-12 10:32:02 -0300

    feat: add input, bagde, banner and appointments

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,80 @@
+import { SearchIcon } from "lucide-react";
 import Header from "./_components/Header";
+import { Input } from "./_components/ui/input";
+import { Button } from "./_components/ui/button";
+import Image from "next/image";
+import { Card, CardContent } from "./_components/ui/card";
+import { Badge } from "./_components/ui/badge";
+import { Avatar, AvatarImage } from "./_components/ui/avatar";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <div className="p-5">
+        <h1 className="text-xl font-bold py-1">Olá, João Vitor</h1>
+        <h2 className="text-sm mt-0.5">Sexta-Feira, 12 de Junho</h2>
+      </div>
+
+      <div className="flex gap-2 px-5 mt-3">
+        <Input placeholder="Buscar Barbearias" />
+        <Button size="icon">
+          <SearchIcon />
+        </Button>
+      </div>
+
+      <div className="px-5 mt-6 flex gap-3 w-full justify-center">
+        <Button variant="outline">
+          <Image src="/iconsscissors.svg" alt="Cabelo" width={20} height={20} />
+          <p className="text-sm">Cabelo</p>
+        </Button>
+
+        <Button variant="outline">
+          <Image src="/iconmustache.svg" alt="Barba" width={20} height={20} />
+          <p className="text-sm">Barba</p>
+        </Button>
+
+        <Button variant="outline">
+          <Image
+            src="/iconrazor-double-edge.svg"
+            alt="Acabamento"
+            width={20}
+            height={20}
+          />
+          <p className="text-sm">Acabamento</p>
+        </Button>
+      </div>
+
+      <div className="w-full px-5 mt-5">
+        <Image
+          src="/Banner-Hero.svg"
+          alt="Agende nos melhores com FSW Barber"
+          width={100}
+          height={100}
+          className="w-full h-full object-cover rounded-2xl"
+        />
+      </div>
+
+      <div className="px-5 mt-2">
+        <h2 className="uppercase text-gray-500 text-sm py-4">Agendamentos</h2>
+        <Card className="flex flex-row gap-2">
+          <CardContent className="flex flex-col gap-2 w-full">
+            <Badge variant="default">Confirmado</Badge>
+            <h3 className="font-semibold text-lg">Corte de Cabelo</h3>
+            <div className="flex items-center gap-2">
+              <Avatar className="">
+                <AvatarImage src="https://utfs.io/f/6b0888f8-b69f-4be7-a13b-52d1c0c9cab2-17m.png" />
+              </Avatar>
+              <p className="text-base">Vintage Barber</p>
+            </div>
+          </CardContent>
+          <CardContent className="flex flex-col text-center items-center justify-center w-[100px]">
+            <p className="text-sm">Junho</p>
+            <h3 className="text-3xl font-bold">12</h3>
+            <p className="text-base">09:30</p>
+          </CardContent>
+        </Card>
+      </div>
     </main>
   );
 }
